bfc43ba6f847a939be3982a03a476e674fcb02c0
Liam Owen <l1amow3n5@yahoo.com>
2018-11-07 13:36:21 -0700

    connected the IBoutlet to the ArtCell file

---
+++ MultiAppProject/View/ArtCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtCell.swift
+//  MultiAppProject
+//
+//  Created by Owen, Liam on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var artImage: UIImageView!
+    @IBOutlet weak var artLabel: UILabel!
+    
+}
